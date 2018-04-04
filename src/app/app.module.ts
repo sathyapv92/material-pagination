c1cddd42575a435edfb1a6afde2edfe86dd6f6dc
sathyapv92 <sathapv92@gmail.com>
2018-04-04 23:26:14 +0530

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { DealEntryComponent } from './deal-entry/deal-entry.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { BlotterComponent } from './blotter/blotter.component';
+import {AgGridModule} from "ag-grid-angular/main";
+import {MatDialogModule} from '@angular/material/dialog';
+import { DComponent, DialogOverviewExampleDialog } from './d/d.component';
+import {RouterModule} from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DealEntryComponent,
+    BlotterComponent,
+    DComponent,
+    DialogOverviewExampleDialog,
+   
+  
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path:"dealentry",
+        component:DealEntryComponent
+      },
+      {
+      path:"",
+      component:BlotterComponent
+    }],{ enableTracing: true }),
+    AgGridModule.withComponents(
+      [])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
